1c6d8b28a01e03b98741b4e2db76ecf683514669
Admin <ksreddykavali@gmail.com>
2025-03-10 12:20:22 +0530

    Removed EMOJI from UI because Hugging face didn't accepted it

--- src/langgraphagenticai/ui/streamlitui/loadui.py
+++ src/langgraphagenticai/ui/streamlitui/loadui.py
@@ -26,7 +26,8 @@ class LoadStreamlitUI:
 
 
     def load_streamlit_ui(self):
-        st.set_page_config(page_title= "🤖 " + self.config.get_page_title(), layout="wide")
+        #st.set_page_config(page_title= "🤖 " + self.config.get_page_title(), layout="wide")  # Hugging face is not taking this emojis
+        st.set_page_config(self.config.get_page_title(), layout="wide")
         st.header("🤖 " + self.config.get_page_title())
         st.session_state.timeframe = ''
         st.session_state.IsFetchButtonClicked = False
